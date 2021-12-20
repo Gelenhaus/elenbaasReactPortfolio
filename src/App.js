08364a5106df117ae24141423e6d7c4b9da945aa
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-12-19 18:32:44 -0700

    almost caught back up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav/index';
 import AboutMe from './components/AboutMe/index';
+import Projects from './components/Projects/index';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Projects></Projects>
         <AboutMe></AboutMe>
       </main>
     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,1 +1,24 @@
-import React from 'react';
+import React from "react";
+import noteTaker from '../../assets/noteTaker/noteTaker.jpg';
+
+function Projects(props) {
+    const currentCategory = {
+        name: "commercial",
+        description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+    };
+    return (
+        <section>
+            <h1>{currentCategory.name}</h1>
+            <p>{currentCategory.name}</p>
+            <div>
+                <img
+                    src={noteTaker}
+                    alt="Commercial Example"
+                    className="img-thumbnail mx-1"
+                ></img>
+            </div>
+        </section>
+    );
+}
+export default Projects;
