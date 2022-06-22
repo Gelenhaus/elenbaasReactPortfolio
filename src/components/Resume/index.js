9f16d72b19f80ee7ab483a4da889ea59194175cb
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-06-22 16:22:45 -0600

    making it pretty

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,10 +4,17 @@ import resume from '../Resume/GEResume2022 (1).pdf';
 
 
 function Resume() {
+
+
     return (
         <section >
+
             <div className="resumeBox">
-                <a href={resume} download>Click to download resume</a>
+
+                <button>
+                    <a href={resume} download>Click to download resume</a>
+                </button>
+
             </div>
         </section>
 
